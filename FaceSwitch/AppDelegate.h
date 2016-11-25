e69d37fff69b3a0a7f1ba1820221708453d1ddd4
clover_htc <clover@haituncun.com>
2016-11-25 10:54:38 +0800

    Initial Commit

---
+++ FaceSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceSwitch
+//
+//  Created by Clover on 11/25/16.
+//  Copyright © 2016 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceSwitch
+//
+//  Created by Clover on 11/25/16.
+//  Copyright © 2016 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
